bb7f5091d85ca9121cb461cae41f9dd8bd59f092
Abdullah KAYA <abdullahky0409@gmail.com>
2024-03-01 16:39:26 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
 
@@ -19,13 +20,13 @@ public class Main {
     }
 
     static int divided (int a, int b) {
-        if (b==0){
-        System.out.println("ikinci sayı 0'dan farklı olmalı!")}
-        return 0;
-
+        if (b == 0) {
+            System.out.println("ikinci sayı 0'dan farklı olmalı!");
+            return 0;
+        }
         int result = a / b;
         System.out.println(" Bölme: " + result);
-        return result;}
+    return result; }
 
     static int power (int a, int b) {
         int result = 1;
@@ -57,17 +58,49 @@ public class Main {
                     + "7 - Dikdörtgen alan ve çevre hesaplama\n"
                     + "8 - Çıkış yap\n";
 
-        }
+            while (true){
+                System.out.println(menu);
+                System.out.println("Bir işlem seç: ");
+                select =scan.nextInt();
+                scan.nextInt();
+                if(select==0){break;
+                }
+            }
+            System.out.print("ilk sayı : ");
+            int a= scan.nextInt();
+            System.out.print("ikinci sayı : ");
+            int b= scan.nextInt();
 
-        while (true){
-        Object case1;
-        case1 ; 
+            switch (select ){
+                case 1:
+                    sum(a,b);
+                    break;
 
-            
+                case 2:
+                    minus(a,b);
+                    break;
+                case 3:
+                    times(a,b);
+                    break;
+                case 4:
+                    divided (a,b);
+                    break;
+                case 5:
+                    System.out.println("üs hesabı : " + power(a,b) );
+                    break;
+                case 6:
+                    System.out.println("Mod işlemi : "+ mod (a,b));
+                    break;
+                case 7:
+                    calc(a,b);
+                    break;
+                default:
+                    System.out.println("Geçersiz bir işlem girdiniz : ");
+
+            }
+            System.out.println("Güle güle ");
     }
-
-
-        }
+    }
 
 
 
